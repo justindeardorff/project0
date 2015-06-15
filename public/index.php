fd357f25c28cb58adf9cb4615e0a5a5efb96c97a
lethaljd <justindeardorff@gmail.com>
2015-06-15 15:07:04 -0400

    update index.php to point to menu2

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
     require("../includes/functions.php");
     
     //open menu.xml file and load into xml variable
-    $xml=simplexml_load_file("../includes/menu.xml") or die("Error: Cannot create object");
+    $xml=simplexml_load_file("../includes/menu2.xml") or die("Error: Cannot create object");
     
     //render main
     render("main.php", ["xml"=>$xml]);
